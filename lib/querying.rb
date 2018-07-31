8d3aaae26c7840a2d13f4800e97a4ac35efec383
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-07-31 22:10:30 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,11 +16,9 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-"SELECT series.title FROM series INNER JOIN characters ON characters.series_id = series.id GROUP BY characters.species ORDER BY MAX(characters.species) DESC LIMIT 1;"
+  "SELECT series.title FROM series INNER JOIN characters ON characters.series_id = series.id GROUP BY characters.species ORDER BY MAX(characters.species) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(books.id) FROM characters INNER JOIN character_books ON character_books.character_id = characters.id JOIN books ON character_books.book_id = books.id GROUP BY characters.name ORDER BY COUNT(books.id) DESC;"
-end
-
 end
